ace78d1abd5b9c6e259cebd81bf533e7536f67bc
ANDREEA-RAMONA PAUL <andreea.ramona.paul@stud.ubbcluj.ro>
2021-01-15 20:38:24 +0200

    Add project files.

---
+++ Paul_Andreea_Lab12/Paul_Andreea_Lab12/Data/IRestService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Paul_Andreea_Lab12.Models;
+using Paul_Andreea_Lab12.Data;
+
+namespace Paul_Andreea_Lab12.Data
+{
+        public interface IRestService
+    {
+        List<ShopList> Items { get; }
+
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+        Task SaveShopListAsync(ShopList item, bool isNewItem = true);
+        Task SaveShopListAsync(ShopList item, bool isNewItem = true);
+    }
+}
